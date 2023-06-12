3f3a229c7e5d85b98fedd3a74339a1f724ebf7b3
ClaraSantosmf <clarasantosmf@gmail.com>
2023-06-11 22:06:20 -0300

    Index em formação. Context é um dicionário que retorna todos valores para pagina inicial

--- naatividade/core/views.py
+++ naatividade/core/views.py
@@ -1,11 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Ativo
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, "index.html")
+    lista_ativos = Ativo.object.filter(active=True)
+    context = {'objectList': lista_ativos}
+    return render(request, "index.html", context)
 
 def processar_formulario(request):
     return
